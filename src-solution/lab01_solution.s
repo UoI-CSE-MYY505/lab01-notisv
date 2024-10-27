e333bf9cec8800f9c1d23030b7290825383e1b79
notisv <56257971+notisv@users.noreply.github.com>
2024-10-27 17:37:14 +0200

    Updated completed lab01

--- src-solution/lab01_solution.s
+++ src-solution/lab01_solution.s
@@ -5,19 +5,16 @@
 
 # These are comments! Anything up to the end of line is ignored
 
-# PRIN TREKSEIS TON KWDIKA BGALE ENA SCREENCAP TON SYMBOL NAVIGATOR (DIPLA APO TO gpr ston ripes)
-# GIA NA EXEIS TIN ANTISTOIXISH LABEL(symbol) - MEMORY ADDRESS
-
-        .data     # An assembler **directive**. What follows is data 
-                  #  so it is stored in the data memory section (0x10000000)
+        .data   # An assembler **directive**. What follows is data 
+                #  so it is stored in the data memory section (0x10000000)
 
 matric: # Words ending with ':' are **labels**
         # The common convention is to write labels starting from the leftmost column.
         # Try to keep label names short
-        .word 2653    # This directive reserves enough space for a word in memory
-                      #  and initializes it with the specific value.
+        .word 30  # This directive reserves enough space for a word in memory
+                  #  and initializes it with the specific value.
 matricplus1:
-        .word 420   
+        .word 0
 
 var1:
         .word 1
@@ -58,18 +55,11 @@ prog:   # Labels in code are used for control flow: if/then/else, loops, etc.
         #  test.
         # Labels do not take up space in memory. They are only used by the assembler
 
-        #li         s0, 100         # Get value 100 into register s0. li - load immediate - CHANGE THIS LINE !!!
-        lw         s0, 0(a0)        # s0 gets the value of matric. a0 has the address of matric
+        lw         s0, 0(a0)
         
         lw         s1, 0(a1)       # s1 gets the value of var1. a1 has the address of var1
-        add        s1, s1,   s0    # s1 = s1 + s0 = var1 + s0
-        sw         s1, 4(a0)       # MEM[matricplus1] = s1 = var1 + s0
-                                   # H TIMH TOU s1 GRAFETAI STH MNHMH 4 8ESEIS PARAKATW (OFFSET = 4)
-                                   # APO EKEI POU DEIXNEI O a0 (DHLADH STO MATRIC)
-                                   # EFOSON TO LABEL matricplus1 EINAI H AMESWS EPOMENH GRAMMH APO TO matric
-                                   # TOTE H DIEF8YNSH TOU EINAI 4 BYTES PARAKATW
-                                   # ARA TELIKA H TIMH tou s1 8A GRAFTEI STH 8ESH MNHMHS TOU matricplus1
-                                   # YOU CAN CONFIRM THIS BY LOOKING AT THE MEMORY TAB IN RIPES
+        add        s1, s1,   s0    # s1 = var1 + s0
+        sw         s1, 4(a0)       # matricplus1 = var1 + s0
         
         sw         s0, 0xc(a2)     # Store s0 to the 4th element of array
                                    # Note the use of offset (0xc = 12 = 3*4)
@@ -78,7 +68,6 @@ prog:   # Labels in code are used for control flow: if/then/else, loops, etc.
                                    # array[1]  - array + 4
                                    # array[2]  - array + 8
                                    # array[3]  - array + 12
-                                   # array[4]  - array + 16
                                      
         addi       t1, a2,   0x10  # Note: address arithmetic.
                                    #  Calculates address of 5th element of array
